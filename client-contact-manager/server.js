61397075ccdfae60cafd8c3df843890c72af3685
Ralph220091994 <ralphd497@gmail.com>
2024-08-09 22:45:22 +0200

    update server.js

--- client-contact-manager/server.js
+++ client-contact-manager/server.js
@@ -10,8 +10,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Debug line to check if the environment variable is loaded correctly
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 .then(() => {
     console.log('Connected to MongoDB Atlas');
 })
